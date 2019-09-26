3c704a5c07ab5c22faae535f31f392be1c491a2b
Morico <moriconomori@gmail.com>
2019-09-26 23:06:18 +0900

    list 表示を追加

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    pools: ''
+  },
+  created: async function() {
+    var pools = [];
+    await axios.get('./api/pools.php')
+      .then(function(res) {
+        pools = res.data;
+      })
+      .catch(function(err) {
+        console.log('Error!');
+      });
+    this.pools = pools;
   }
 })
