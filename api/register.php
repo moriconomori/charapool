b8f40c7ce9986ee6b1c017e4a202f3eba4bbd06f
Morico <moriconomori@gmail.com>
2019-09-27 01:22:54 +0900

    新規登録処理を追加

---
+++ api/register.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once('../config/database.php');
+
+$pdo = getPdo();
+
+$playerName = $_POST['playerName'];
+$heros = $_POST['hero'];
+
+$sql = "INSERT INTO charpool (player, hero) VALUES";
+$sql .= implode(",", array_fill(0, count($heros), "(?, ?)"));
+
+$values = array();
+
+for ($i = 0; $i < count($heros); $i++) {
+    $values[] = $playerName;
+    $values[] = $heros[$i];
+}
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute($values);
