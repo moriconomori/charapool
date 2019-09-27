66b8965ed67e1cdf41bbcab3845098238cbe9b75
Morico <moriconomori@gmail.com>
2019-09-27 13:47:55 +0900

    gtag を追加

---
+++ gtag.js
@@ -0,0 +1,10 @@
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=UA-148912327-1"></script>');
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+  dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', 'UA-148912327-1');
