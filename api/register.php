8871235d86d8773bfb41d27e23edca9520f64e2c
Morico <moriconomori@gmail.com>
2019-09-29 02:58:55 +0900

    得意なヒーローの登録、表示機能の追加

--- api/register.php
+++ api/register.php
@@ -5,20 +5,26 @@ require_once('../config/database.php');
 $pdo = getPdo();
 
 $playerName = $_POST['playerName'];
-$heros = $_POST['hero'];
-
-$sql = "INSERT INTO charpool (player, hero) VALUES";
-$sql .= implode(",", array_fill(0, count($heros), "(?, ?)"));
+$heros = $_POST['heros'];
 
 $values = array();
 
-for ($i = 0; $i < count($heros); $i++) {
-    $values[] = $playerName;
-    $values[] = $heros[$i];
+$heroNum = 0;
+foreach ($heros as $item) {
+    if ($item['hasHero'] === "true") {
+        $values[] = $playerName;
+        $values[] = $item['name'];
+        $values[] = $item['specialty'];
+        $heroNum++;
+    }
 }
+
+$sql = "INSERT INTO charpool (player, hero, specialty) VALUES";
+$sql .= implode(",", array_fill(0, $heroNum, "(?, ?, ?)"));
 
 $stmt = $pdo->prepare($sql);
 $stmt->execute($values);
 
 header('Location: ../');
+
 exit;
